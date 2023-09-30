1938e8705d10a39f44b315c01ac78dee65fe1ed2
Eljefe Wappo <eljefe.wappo@gmail.com>
2023-09-30 12:41:55 -0400

    second try, does not parse the  Repeat="12" value

--- time_calculator.py
+++ time_calculator.py
@@ -1,13 +1,13 @@
-import sys
 import re
+import sys
 
-def convert_to_seconds(time_str):
-    # Convert time string to seconds
-    hours, minutes, seconds = map(int, time_str.split(":"))
-    total_seconds = hours * 3600 + minutes * 60 + seconds
-    return total_seconds
+def convert_to_seconds(duration_str):
+    # Extract numeric value from the string and convert to float
+    seconds = float(re.search(r'\d+', duration_str).group())
+    return seconds
 
 def main():
+    # Check if a file name is provided as a command line argument
     if len(sys.argv) != 2:
         print("Usage: python script.py <filename>")
         sys.exit(1)
@@ -18,11 +18,13 @@ def main():
         with open(filename, 'r') as file:
             content = file.read()
 
-            # Use regular expressions to find and extract durations
-            durations = re.findall(r'Duration="(\d+:\d+:\d+)"', content)
+            # Find all matches for Duration, OnDuration, and OffDuration
+            durations = re.findall(r'Duration="([^"]+)"', content)
+            on_durations = re.findall(r'OnDuration="([^"]+)"', content)
+            off_durations = re.findall(r'OffDuration="([^"]+)"', content)
 
-            # Convert durations to seconds and sum them
-            total_seconds = sum(map(convert_to_seconds, durations))
+            # Convert and sum the durations
+            total_seconds = sum(map(convert_to_seconds, durations + on_durations + off_durations))
 
             # Convert total seconds to hours, minutes, seconds
             hours, remainder = divmod(total_seconds, 3600)
@@ -33,7 +35,7 @@ def main():
     except FileNotFoundError:
         print(f"File '{filename}' not found.")
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
